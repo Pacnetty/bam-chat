fdd0bc90a0024e7934b29ff59917a1ebbedefdb1
Brian Smith <=>
2014-02-09 12:07:59 -0500

    Made action into 1 command, and removed getBlocklandID and put where needed

--- index.js
+++ index.js
@@ -4,35 +4,10 @@ var net 		= require('net'),
 	clients		= require('./clients'),
 	request		= require('request');
 
-function getBlocklandID(name, ip, callback) {
-	var payload = { "NAME": name, "IP": ip };
-	request.post( { url: 'http://auth.blockland.us/authQuery.php', form: payload }, function( error, response, body) {
-		var spl = response['body'].split(' ');
-		if( spl[0] == "NO" ) {
-			callback( -1 );
-		}
-		else {
-			callback( parseInt( spl[1] ) );
-		}
-	} );
-}
-
 function debugString(msg) {
 	var time = new Date().toISOString().replace(  /T/, ' ' ).replace( /\..+/, '' );
 	console.log( "[" + time + "] " + msg );
 }
-
-var action_json = {
-	"type": "action",
-	"data": {
-		"action": "@ignore_value",
-		"name": "@ignore_value"
-	}
-};
-var action_data = {
-	"action": '["data"]["action"]',
-	"name": '["data"]["name"]'
-};
 
 var global_manager = new clients.ClientManager();
 
@@ -44,29 +19,32 @@ var server = net.createServer( function(conn) {
 			debugString( "Message: " + line );
 			if( "type" in data ) {
 				switch( data["type"] ) {
-					case "action":
-						formatter( action_json, data, action_data, function( match, action_obj ) {
-							if( match ) {
-								switch( action_obj.action ) {
-									case "join":
-										var client = new clients.Client( action_obj.name, conn );
+					case "join":
+						var client = new clients.Client( data["data"]["name"], conn );
 										
-										global_manager.add_client( client );
-										getBlocklandID( action_obj.name, conn.remoteAddress, client.setBLID );
-								
-										debugString( "User \"" + action_obj.name + "\" authenticated." );
-										break;
-
-									case "leave":
-										// handle leaving different than disconnecting?
-										global_manager.remove_conn( conn ); // just do this for now.
-										break;
-								}
-							}
-						});
-						// handle joining and leaving
-						break;
+						global_manager.add_client( client );
 					
+						var payload = { "NAME": client.name, "IP": conn.remoteAdddress };
+						request.post( { url: 'http://auth.blockland.us/authQuery.php', form: payload }, function( error, response, body) {
+							var spl = response['body'].split(' ');
+							var id = parseInt( spl[1] );
+							if( spl[1] == undefined ) {
+								debugString( "Couldn't fetch BLID for " + client.name + ", disconnecting..." );
+								global_manager.remove_conn( conn );
+								conn.end();
+							}
+							else {
+								debugString( "User \"" + data["data"]["name"] + "\" authenticated with BL_ID " + id.toString() + "." );
+								client.blid = id;
+							}
+						} );
+						break;
+						
+					case "leave":
+						// handle leaving different than disconnecting?
+						global_manager.remove_conn( conn ); // just do this for now.
+						conn.end();
+						break;
 					case "group":
 						// handling joining, leaving, banning, and kicking from groups.
 						break;
@@ -81,7 +59,7 @@ var server = net.createServer( function(conn) {
 				}
 			}
 		} catch ( e ) {
-			debugString( "Caught JSON Error: " + e );
+			debugString( "Caught Error: " + e );
 		}
 
 	});
